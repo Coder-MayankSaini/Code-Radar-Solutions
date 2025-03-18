d8ec93e65a2233790f77d0012c98b450edbe5d0b
Mayank Saini <officialmail.mayanksaini@gmail.com>
2025-03-18 12:20:36 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,20 +4,20 @@ int main(){
 int a,b,o,output;
 scanf("%d%d%c",&a,&b,&o);
 
-if(o == +){
+if(o == '+'){
 output = a+b;
 }
-else if (o == -){
+else if (o == '-'){
     output = a-b;
 }
-else if (o == * ){
+else if (o == '*' ){
     output = a*b;
 }
-else if(o == / ){
+else if(o == '/' ){
     output = a/b;
 }
 else{
-    output = "error";
+    printf("error");
 }
 
 printf("%d",output);
